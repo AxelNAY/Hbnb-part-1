b853b250f694e007be9ce3a2a86f5f1f8d0a4a49
edemdj <edemdj.edemdjossou1997@gmail.com>
2024-06-26 15:57:47 +0200

    import module in manage.py

--- solutions/solution-00/app.py
+++ solutions/solution-00/app.py
@@ -5,13 +5,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///development.db'
 db = SQLAlchemy(app)
 
-# Assurez-vous d'importer les modèles après l'initialisation de db
 from src.models.user import User
 from src.models.city import City
 from src.models.country import Country
 from src.models.place import Place
 from src.models.review import Review
 
-# Créez toutes les tables
+
 with app.app_context():
     db.create_all()
--- solutions/solution-00/manage.py
+++ solutions/solution-00/manage.py
@@ -1,9 +1,17 @@
 """ Entry point for the application. """
 
 from flask.cli import FlaskGroup
+from src import db
+from src.models.user import User
+from src.models.city import City
 from src import create_app
 
 cli = FlaskGroup(create_app=create_app)
+@cli.command('create_db')
+def create_db():
+    db.drop_all()
+    db.create_all()
+    print("Database tables created")
 
 
 if __name__ == "__main__":
